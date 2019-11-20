a35af63a3832b330d99b2ee779d694baeba1e415
Téo Schaffner <teo.schaffner@he-arc.ch>
2019-11-20 15:50:09 +0100

    oewrnfionpomglkbbwlfneiulvfiseblizfshfib

--- cuisinator/resources/views/recettes/index.blade.php
+++ cuisinator/resources/views/recettes/index.blade.php
@@ -18,20 +18,23 @@
                             </form>
                         </div>
 
+                        @php
+                        $i = 0;
+                        @endphp
                         @foreach($aliments as $a)
-                            <figure class="figure col-lg-4 col-md-12 col-sm-3 col-xs-6 draggable" draggable="true" ondragstart="drag(event)">
+                            <figure class="figure col-lg-4 col-md-12 col-sm-3 col-xs-6 draggable" draggable="true" ondragstart="drag(event)" id="{{$loop->index}}" ondrop="return false;">
                                 <div>
                                     <img src="{{ URL::asset('photos-aliments/' . ((!is_null($a->nom_photo))? $a->nom_photo : "default.jpg" )) }}"
-                                         alt="{{$a->nom}}" class="figure-img rounded aliment-image-icon" />
+                                         alt="{{$a->nom}}" class="figure-img rounded aliment-image-icon" ondrop="return false;" />
                                 </div>
 
-                                <figcaption class="figure-caption">{{$a->nom}}</figcaption>
+                                <figcaption class="figure-caption" ondrop="return false;">{{$a->nom}}</figcaption>
                             </figure>
                         @endforeach
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 container spaced-inside">
+            <div class="col-md-3 container spaced-inside fill-parent">
                 <div class="container fill-parent">
                     <div class="row p-3 border bg-light rounded-lg fill-parent" id="liste-aliments-possedes" ondrop="drop(event)" ondragover="allowDrop(event)">
 
@@ -53,15 +56,18 @@
     <script type="text/javascript">
         function allowDrop(ev) {
             ev.preventDefault();
+            ev.dataTransfer.dropEffect = "move";
         }
 
         function drag(ev) {
-            ev.dataTransfer.setData("text", ev.target.id);
+            ev.dataTransfer.setData("text/plain", ev.target.id);
         }
 
         function drop(ev) {
             ev.preventDefault();
-            var data = ev.dataTransfer.getData("text");
+            // Get the id of the target and add the moved element to the target's DOM
+            var data = ev.dataTransfer.getData("text/plain");
+            console.log(data);
             ev.target.appendChild(document.getElementById(data));
         }
     </script>
