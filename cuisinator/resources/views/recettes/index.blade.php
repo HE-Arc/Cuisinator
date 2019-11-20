7c9a543a1f144cbd16b637dab138cdf7787aa55e
Tristan Seuret <tristan.seuret@he-arc.ch>
2019-11-20 15:49:43 +0100

    Tried to fix column main page

--- cuisinator/resources/views/recettes/index.blade.php
+++ cuisinator/resources/views/recettes/index.blade.php
@@ -7,34 +7,35 @@
         <h1 class="pb-4 mt-4 mb-2 border-bottom text-center title">Cuisinator</h1>
 
         <div class="row fill-parent">
-            <div class="col-md-3 container spaced-inside min-vh-100">
-                <div class="container min-vh-100">
-                    <div class="row p-3 border bg-light rounded-lg" id="liste-aliments">
-
+            <div class="col-md-3 container spaced-inside fill-parent">
+                <div class="container fill-parent">
+                    <div class="row p-3 border bg-light rounded-lg fill-parent" id="liste-aliments">
+                        <div class="row w-100 mx-auto">
                         <div class="col-12 spaced-bottom">
                             <form class="form-inline">
                                 <input class="form-control col" type="search" placeholder="Chercher un aliment" />
                                 <button class="form-control btn btn-success justify-content-end" type="submit"><i class="fas fa-search"></i></button>
                             </form>
                         </div>
-
+                        </div>
+                        
+                        <div class="row mb-auto">
                         @foreach($aliments as $a)
                             <figure class="figure col-lg-4 col-md-12 col-sm-3 col-xs-6">
-                                <div>
+                                <div class="">
                                     <img src="{{ URL::asset('photos-aliments/' . ((!is_null($a->nom_photo))? $a->nom_photo : "default.jpg" )) }}"
                                          alt="{{$a->nom}}" class="figure-img rounded aliment-image-icon" />
                                 </div>
-
                                 <figcaption class="figure-caption">{{$a->nom}}</figcaption>
                             </figure>
-                        @endforeach
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 container spaced-inside">
+            <div class="col-md-3 container spaced-inside fill-parent">
                 <div class="container fill-parent">
                     <div class="row p-3 border bg-light rounded-lg fill-parent" id="liste-aliments-possedes">
-                        
                     </div>
                 </div>
             </div>
